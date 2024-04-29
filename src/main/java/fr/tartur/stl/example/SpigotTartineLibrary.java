6b5e3caee94e229709a80d3b43479256e5c6e2e7
tarturr <tartur.dev@gmail.com>
2024-04-29 20:22:24 +0200

    Initialized plugin project.

---
+++ src/main/java/fr/tartur/stl/example/SpigotTartineLibrary.java
@@ -0,0 +1,18 @@
+package fr.tartur.stl.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpigotTartineLibrary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
